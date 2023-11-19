9e6cc3fd71484726a7608420767c045b3b995901
Dmitry <dimaermilov38@gmail.com>
2023-11-19 16:59:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$number = 5;
+$num = 3;
+$sum = $number + $num;
+$str = "Переменая";
+echo $str . ': ' . $number . $num;
+echo '<br/>' . $sum;
+
+$rand = mt_rand(1, 100);
+echo '<br/><br/>' . $rand;
+
